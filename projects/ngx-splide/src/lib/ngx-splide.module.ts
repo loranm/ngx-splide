d795d8ed0a7332c726811d5663716533ee6a3030
loran <laurent.muller@wizbii.com>
2021-08-03 09:34:40 +0200

    master: Removed BrowserModule

--- projects/ngx-splide/src/lib/ngx-splide.module.ts
+++ projects/ngx-splide/src/lib/ngx-splide.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxSplideComponent } from './ngx-splide.component';
-import { NgxSplideSlideComponent } from './ngx-splide-slide.component';
+import { NgModule } from "@angular/core";
+import { NgxSplideComponent } from "./ngx-splide.component";
+import { NgxSplideSlideComponent } from "./ngx-splide-slide.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-    declarations: [ NgxSplideComponent, NgxSplideSlideComponent ],
-    imports: [ BrowserModule ],
-    exports: [ NgxSplideComponent, NgxSplideSlideComponent ]
+    imports: [CommonModule],
+    declarations: [NgxSplideComponent, NgxSplideSlideComponent],
+    exports: [NgxSplideComponent, NgxSplideSlideComponent],
 })
 export class NgxSplideModule {}
